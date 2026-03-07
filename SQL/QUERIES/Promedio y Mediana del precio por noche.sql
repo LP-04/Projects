0e0b53f783a6a500a9ca38e83674fecf4e63b62e
LP-04 <121700676+LP-04@users.noreply.github.com>
2026-03-06 22:34:09 -0500

    Enhance SQL query to include average reviews by neighborhood
    Added a query to calculate average reviews per neighborhood.

--- SQL/QUERIES/Promedio y Mediana del precio por noche.sql
+++ SQL/QUERIES/Promedio y Mediana del precio por noche.sql
@@ -1,2 +1,13 @@
 SELECT avg(price) as precio_promedio, median(price) as mediana
 FROM listings_clean
+
+
+with a as (
+  SELECT sum(number_of_review) as total
+  from listings_clean
+)
+SELECT neighbourhood, round(100*((sum(number_of_review)*1.0)/total),2) as reseñas_promedio
+FROM listings_clean l
+CROSS JOIN a 
+GROUP BY neighbourhood
+ORDER by reseñas_promedio desc
