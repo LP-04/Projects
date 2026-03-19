192459a5f21d2bb8a3bbe45f78304f07bb51a72b
LP-04 <121700676+LP-04@users.noreply.github.com>
2026-03-19 13:07:51 -0500

    Update Promedio y Mediana del precio por noche.sql

--- SQL/QUERIES/Promedio y Mediana del precio por noche.sql
+++ SQL/QUERIES/Promedio y Mediana del precio por noche.sql
@@ -1,18 +1,2 @@
 SELECT avg(price) as precio_promedio, median(price) as mediana
 FROM listings_clean
-
-
-with a as (
-  SELECT sum(number_of_review) as total
-  from listings_clean
-)
-SELECT neighbourhood, round(100*((sum(number_of_review)*1.0)/total),2) as reseñas_promedio
-FROM listings_clean l
-CROSS JOIN a 
-GROUP BY neighbourhood
-ORDER by reseñas_promedio desc
-
-
-SELECT DISTINCT host_id, host_name, calculated_host_
-FROM listings_clean l
-ORDER by calculated_host_ desc
